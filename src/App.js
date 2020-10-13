0830254e1074b77a1ed7941effabe80e82d3a8e0
vagnersantos1996 <vagnersantos1996@hotmail.com>
2020-10-13 14:21:22 -0300

    hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+	state = {repositories: []}
+*/
+
+export default function App() {
+	const [repositories, setRepositories] = useState([]);
+
+	//primeiro parametro -> o que deve ser feito
+	//segundo parametro -> quando deve ser feito. Recebe um array de variáveis que, quando alteradas, ativam a função.
+	//Se não houver nada ( "[]" ), ele executará quando iniciar a página, como um "componentDidMount"
+	useEffect(() => {
+		async function fetchData() {
+			const response = await fetch('https://api.github.com/users/vagnersantos1996/repos');
+			const data = await response.json();
+			setRepositories(data);
+		}
+
+		fetchData();
+	}, []);
+
+	useEffect(() => {
+		const filtered = repositories.filter(repo => repo.favorite)
+		document.title = `Você tem ${filtered.length} favorito(s)`;
+	}, [repositories]);
+
+	//n pode usa arrow function
+	function handleFavorite(id) {
+		const newRepositories = repositories.map(repo => {
+			return repo.id === id ? {...repo, favorite: !repo.favorite} : repo
+		});
+
+		setRepositories(newRepositories);
+	}
+
+	return (
+		<>
+			<ul>
+				{repositories.map(repo => <li key={repo.id}>
+					{repo.name}
+					{repo.favorite && <span>(Favorito)</span>}
+					<button onClick={() => handleFavorite(repo.id)}>{repo.favorite ? "Desfavoritar" : "Favoritar"}</button>
+				</li>)}
+			</ul>
+		</>
+	)
 }
-
-export default App;
